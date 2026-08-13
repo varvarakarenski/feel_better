530a262ba94ffa48d2e882649b6aa075628d61fe
Varvara Karenski <varvarakarenski@gmail.com>
2026-08-13 01:25:12 -0400

    Initial site with trimmed background video
    + squashed history to fix HUGE bg video issues

---
+++ pizzazz.js
@@ -0,0 +1,34 @@
+const button = document.getElementById('playAudioBtn');
+const audio = document.getElementById('myAudio');
+
+if (button && audio) {
+  button.addEventListener('click', () => {
+    audio.currentTime = 0;
+
+    audio.play();
+  });
+}
+
+const chestButton = document.getElementById('chestButton');
+const closedScroll = document.getElementById('closed-scroll');
+const closedScrollButton = document.getElementById('closedScrollButton');
+const scroll = document.getElementById('scroll');
+const scrollButton = document.getElementById('scrollButton');
+const blankPage = document.getElementById('blank-page');
+const stories = document.getElementById('stories')
+
+chestButton.addEventListener('click', () => {
+  closedScroll.classList.add('show');
+  chestButton.style.display = 'none';
+});
+
+closedScrollButton.addEventListener('click', () => {
+  scroll.classList.add('show');
+  closedScroll.classList.remove('show');
+});
+
+scrollButton.addEventListener('click', () => {
+  stories.classList.add('show');
+  scroll.classList.remove('show');
+});
+
